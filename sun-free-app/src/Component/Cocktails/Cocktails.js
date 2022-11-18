3966dd9b69a0cc6cc9349acebf4caada8ebc5ef3
Federica Costantino <ricacostt@gmail.com>
2022-11-18 15:53:53 +0000

    IT'S WORKINGgit add . display image, name, istructions of cocktails

--- sun-free-app/src/Component/Cocktails/Cocktails.js
+++ sun-free-app/src/Component/Cocktails/Cocktails.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 //make component function
 export default function Cocktails({id}) {
   //top level of component useState
-  const [cocktail, setCocktail] = useState([]);
+  const [cocktail, setCocktail] = useState(null);
 
   useEffect(() => {
     async function getCocktails() {
@@ -12,14 +12,27 @@ export default function Cocktails({id}) {
       );
       const data = await response.json();
       console.log(data);
-      setCocktail(data);
+      //setCocktail(data);
+      setCocktail(data.drinks[0])
     }
     getCocktails(id);
   }, [id]);
   
-  return <div>
-    <h2 style={{color: "white"}}>istructions:{cocktail.instructions}</h2>
-  </div>;
+  if (cocktail === null) {
+     return <p>please wait</p>;
+  } 
+  return (
+    <div>
+      <h2 style={{ color: "white" }}>name: {cocktail.strDrink}</h2>
+      <p style={{ color: "white" }}>It is {cocktail.strAlcoholic}!</p>
+      <img
+        style={{ width: "70%", borderRadius: "20px" }}
+        src={cocktail.strDrinkThumb}
+      ></img>
+      <h4 style={{ color: "white" }}>{cocktail.strInstructions}</h4>
+      <h2 style={{ color: "white" }}>Buon Appetito!</h2>
+    </div>
+  );
 }
 //create API fetch function
 //return API data to render from the function
